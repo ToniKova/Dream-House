9e666e693eefa3b34f42e487e7a9112cceeda847
Anton Anton <Toni95Kova@mail.com>
2023-09-05 18:53:00 +0300

    commit12

--- build/js/index.bundle.js
+++
@@ -1,1 +0,0 @@
-new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
