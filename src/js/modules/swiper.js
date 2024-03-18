2a23a8ac5c318f1558df8a8fefbe0ef3f737bdcf
Anton Anton <Toni95Kova@mail.com>
2024-03-18 13:56:29 +0200

    commit 19

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,99 +1,154 @@
-// import { render } from 'sass';
+const breakpoint = window.matchMedia('(min-width: 1023px)')
+const mainSwiper = document.querySelector('.swiper');
+let mySwiper;
 
-export default swiper;
+if (breakpoint.matches === true) {
+  enableSwiper()
+  
+} else if (breakpoint.matches === false ) {
+  if ( mySwiper !== undefined ) {
+    mySwiper.destroy( true, true )
+  } 
+  mainSwiper.classList.add('swiper-scroll')
 
-// const windowWidth = window.innerWidth
-// if (windowWidth > 1023) {
-// }
+}
+function enableSwiper  () {
+  mySwiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+    parallax: true,
+    speed: 700,
+    effect:'fade',
+    mousewheel: {
+      sensitivity: 1,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        // effect:'fade',
+        // enabled: true,
+        // watchOverflow:false
+      },
+      220: {
+        // effect:false,
+        // enabled: false,
+      },
+    },
+  });
+}
+
+
 // window.addEventListener('resize', () => {
 //   const widthBody = document.body.clientWidth;
 //   if (widthBody > 1023) {
-//     funk();
+//     InitSwiper();
 //   }
 // });
-funk();
-function funk() {
-  // const swiper = new Swiper('.swiper', {
-  //   direction: 'vertical',
-  //   loop: true,
-  //   parallax: true,
-  //   speed: 700,
-  //   mousewheel: {
-  //     sensitivity: 1,
-  //   },
-  //   keyboard: {
-  //     enabled: true,
-  //   },
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     clickable: true,
-  //   },
-  //   breakpoints: {
-  //     1024: {
-  //       // effect:'fade',
-  //       enabled: true,
-  //       // watchOverflow:false
-  //     },
-  //     220: {
-  //       // effect:false,
-  //       enabled: false,
-  //     },
-  //   },
-  // });
-}
-const swiper = new Swiper('.swiper', {
-  direction: 'vertical',
-  loop: true,
-  parallax: true,
-  speed: 700,
+// // InitSlider();
+// function InitSwiper() {
+//   const swiper = new Swiper('.swiper', {
+//     direction: 'vertical',
+//     loop: true,
+//     parallax: true,
+//     speed: 700,
+//     effect:'fade',
+//     mousewheel: {
+//       sensitivity: 1,
+//     },
+//     keyboard: {
+//       enabled: true,
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     breakpoints: {
+//       1024: {
+//         // effect:'fade',
+//         enabled: true,
+//         // watchOverflow:false
+//       },
+//       220: {
+//         // effect:false,
+//         enabled: false,
+//       },
+//     },
+//   });
+//   // if (window.matchMedia('(max-width: 1023px)').matches) {
+//   //   swiper.destroy(true, true);
+//   // }
+// }
 
-  mousewheel: {
-    sensitivity: 1,
-  },
-  keyboard: {
-    enabled: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
 
-  breakpoints: {
-    1024: {
-      // effect:'fade',
-      enabled: true,
-      // watchOverflow:false
-    },
-    320: {
-      // effect:false,
-      enabled: false,
-    },
-  },
-});
+
+
+// const swiper = new Swiper('.swiper', {
+//   direction: 'vertical',
+//   loop: true,
+//   parallax: true,
+//   speed: 700,
+//   effect:'fade',
+
+//   mousewheel: {
+//     sensitivity: 1,
+//   },
+//   keyboard: {
+//     enabled: true,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   breakpoints: {
+//     1024: {
+//       // allowSlidePrev: true,
+//       // allowSlideNext: true,
+//       // effect:'fade',
+//       enabled: true,
+//       // watchOverflow:false
+//     },
+//     320: {
+//       // allowSlidePrev: false,
+//       // allowSlideNext: false,
+//       // effect:false,
+//       enabled: false,
+//     },
+//   },
+// });
 
 // if (window.matchMedia('(max-width: 1023px)').matches) {
 //   swiper.destroy(true, true);
 // }
 
-const mainSwiper = document.querySelector('.swiper');
-window.addEventListener('resize', addScrollBody);
-addScrollBody();
+// const mainSwiper = document.querySelector('.swiper');
+// window.addEventListener('resize', addScrollBody);
+// addScrollBody();
 
-function addScrollBody() {
-  // const pagination = document.querySelector('.swiper-pagination');
-  const widthBody = document.body.clientWidth;
-  // console.log(widthBody)
-  if (widthBody <= 1024) {
-    mainSwiper.classList.add('swiper-scroll');
-    // location.reload()
-    // pagination.classList.add('pagination-none')
-    // console.log(mainSwiper);
-  } else {
-    mainSwiper.classList.remove('swiper-scroll');
-    // pagination.classList.remove('pagination-none')
-  }
-}
+// function addScrollBody() {
+//   // const pagination = document.querySelector('.swiper-pagination');
+//   const widthBody = document.body.clientWidth;
+//   // console.log(widthBody)
+//   if (widthBody <= 1024) {
+//     mainSwiper.classList.add('swiper-scroll');
+//     // location.reload()
+//     // pagination.classList.add('pagination-none')
+//     // console.log(mainSwiper);
+//   } else {
+//     mainSwiper.classList.remove('swiper-scroll');
+//     // pagination.classList.remove('pagination-none')
+//   }
+// }
 
 // window.addEventListener('click', (e) => {
 //   console.log(e.target)
 // })
+
+
+export default swiper;
